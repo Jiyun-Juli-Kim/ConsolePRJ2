59a52bff3898c6ef2b986368e09e3a26b1e32a58
Jiyun-Juli-Kim <juliyooni@gmail.com>
2024-08-16 14:29:51 +0900

    11
    11

--- room.cs
+++ room.cs
@@ -5,7 +5,28 @@ public class Room
 	public int Level { get; private set; }
 	public int MaxLevel { get; } = 5;
 
-
 	private int[] upgCost = { 200, 200, 300, 400 };
-	private int currBalance;
+
+	public Room()
+	{
+		Level = 1;
+	}
+
+	public bool Upgrade()
+	{
+		if (Level == MaxLevel)
+		{
+			//이미 최대레벨
+			return false;
+		}
+
+		int upgCost = upgCost[Level-1];
+
+		if (Player.Balance >= upgCost) 
+		{
+
+
+	}
+
+
 }
