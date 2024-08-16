fd0c8bbf05c089a7f570c20b6e93502ce36ac8dd
Jiyun-Juli-Kim <juliyooni@gmail.com>
2024-08-16 12:59:36 +0900

    10
    10

---
+++ player.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public class Player
+{
+	string Name { get; set; }
+	int Balance { get; set; }	
+}
---
+++ room.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Room
+{
+	public int Level { get; private set; }
+	public int MaxLevel { get; } = 5;
+
+
+	private int[] upgCost = { 200, 200, 300, 400 };
+	private int currBalance;
+}
